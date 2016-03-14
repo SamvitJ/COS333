db90f83328774ee06e99194449da163fe5eedd05
SamvitJ <samvit.jain@gmail.com>
2016-03-14 12:02:08 -0700

    Configured app.js GET handler to return all data in interviewers collection.

--- node-server/app.js
+++ node-server/app.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var app = express();
 
+app.locals.pretty = true;
 app.set('port', (process.env.PORT || 5000))
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    var MongoClient = require('mongodb').MongoClient;
+    MongoClient.connect((process.env.MONGOLAB_URI || 'mongodb://localhost:27017/database'), function(err, db) {
+        if (err) {
+            throw err;
+        }
+        db.collection('interviewers').find().toArray(function(err, result) {
+            if (err) {
+                throw err;
+            }
+            res.send('Interviewers:\n' + JSON.stringify(result, null, 4));
+        });
+    });
 });
 
 app.listen(app.get('port'), function () {
