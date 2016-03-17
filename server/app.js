c4ad719e955d417980dcfc0cf790572b4cd4a6e4
SamvitJ <samvit.jain@gmail.com>
2016-03-17 10:17:10 -0700

    Resolved issue with unclosed mongo connection.

--- server/app.js
+++ server/app.js
@@ -7,7 +7,8 @@ var express     = require('express');
 app.locals.pretty = true;
 app.set('port', (process.env.PORT || 5000))
 
-mongoose.createConnection(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/database', function (err) {
+var dbURI = 'mongodb://localhost:27017/database'
+mongoose.connect(process.env.MONGOLAB_URI || dbURI, function (err) {
     if (err) {
         throw err;
     }
@@ -22,24 +23,19 @@ app.get('/pubnub', function (req, res) {
 });
 
 app.get('/', function (req, res) {
-    mongoose.connect((process.env.MONGOLAB_URI || 'mongodb://localhost:27017/database'), function(err, db) {
-        if (err) {
-            throw err;
-        }
-        Interviewer.find({}, function (err, docs) {
-            res.write('Interviewers:\n');
-            res.write(JSON.stringify(docs, null, 4));
-            res.end();
-        });
-        /* db.collection('interviewers').find().toArray(function(err, result) {
-            if (err) {
-                throw err;
-            }
-            res.send('Interviewers:\n' + JSON.stringify(result, null, 4));
-        }); */
+    Interviewer.find({}, function (err, docs) {
+        res.write('Interviewers:\n');
+        res.write(JSON.stringify(docs, null, 4));
+        res.end();
     });
 });
 
 app.listen(app.get('port'), function () {
     console.log('Node app running at localhost:' + app.get('port'));
+});
+
+process.on('SIGINT', function() {
+    mongoose.connection.close(function() {
+        process.exit(0);
+    });
 });
