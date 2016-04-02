6d2a9ac3285c46cd5b3f77985648f08765789635
Joshin <JoshuaShin96@gmail.com>
2016-04-02 01:16:42 -0400

    Hangouts Test

--- client/js/demo.js
+++ client/js/demo.js
@@ -10,9 +10,26 @@
     if (event.isApiReady === true) {  
       console.log("API Ready"); 
       console.log(gapi.hangout.getHangoutUrl())
-      xhttp.open("POST", "http://shrouded-stream-84278.herokuapp.com/api/hangouts", true);
-      xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-      xhttp.send("url=fdsa");
+      // xhttp.open("POST", "http://shrouded-stream-84278.herokuapp.com/api/hangouts", true);
+      // xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+      // xhttp.send("url=fdsa");
+
+      var http = new XMLHttpRequest();
+      var url = "http://shrouded-stream-84278.herokuapp.com/api/hangouts";
+      var params = "url=fdsa";
+      http.open("POST", url, true);
+
+      //Send the proper header information along with the request
+      http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+      http.setRequestHeader("Content-length", params.length);
+      http.setRequestHeader("Connection", "close");
+
+      // http.onreadystatechange = function() {//Call a function when the state changes.
+      //     if(http.readyState == 4 && http.status == 200) {
+      //         alert(http.responseText);
+      //     }
+      // }
+      http.send(params);
     } 
   };  
     
