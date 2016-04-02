51fbcb4e631ee1c5ee210679f84af406a6c0195b
Joshin <JoshuaShin96@gmail.com>
2016-04-02 01:20:42 -0400

    Hangouts Test

--- server/app.js
+++ server/app.js
@@ -8,6 +8,12 @@ var express                = require('express');
 
 var app = express();
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();      
+}); 
+
 // server/DB config
 app.set('port', (process.env.PORT || 5000))
 
