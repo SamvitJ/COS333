a463fd0d7b73f1715d5b048b5e03b86629f8b195
SamvitJ <samvit.jain@gmail.com>
2016-04-03 23:06:26 -0400

    Started adding unit tests.
    Added dummy test to controllersSpec.js.
    Added route testing specs (commented out; FIX).
    Added servicesSpec.js file.

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -1,16 +1,31 @@
 'use strict';
 
-/* jasmine specs for controllers go here */
+/* Dummy test */
+describe("A suite", function() {
+  it("contains spec with an expectation", function() {
+    expect(true).toBe(true);
+  });
+});
+
+/* Routing tests */
 describe('Coach routing', function() {
 
-  var request = require('request');
+  beforeEach(angular.mock.module('coachApp'));
 
-  beforeEach(module('coachApp'));
+  /* var request = require('request');
+  var base_url = "http://localhost:5000/";
 
-  it("test site.html routing", function(done) {
-    request("http://localhost:8000/site.html", function(error, response, body) {
+  it("site.html", function(done) {
+    request.get(base_url + "site.html", function(error, response, body) {
+      expect(response.statusCode).toBe(200);
       done();
     });
   });
 
+  it("listings.html", function(done) {
+    request.get(base_url + "listings.html", function(error, response, body) {
+      expect(response.statusCode).toBe(200);
+      done();
+    });
+  }); */
 });
