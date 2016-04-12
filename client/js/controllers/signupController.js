ce8426d38e49c34c0d927c6604dd13e3318ec747
SamvitJ <samvit.jain@gmail.com>
2016-04-12 19:27:09 -0400

    Moved google user account data script to controller file.

--- client/js/controllers/signupController.js
+++ client/js/controllers/signupController.js
@@ -2,6 +2,20 @@ var signupController = angular.module('signupController', []);
 
 signupController.controller('SignupCtrl', ['$scope', 'User', function ($scope, User) {
 
+  window.getGoogleData = function (googleUser) {
+    // Useful data for your client-side scripts:
+    var profile = googleUser.getBasicProfile();
+    var authResp = googleUser.getAuthResponse();
+
+    $scope.name = profile.getName();
+    $scope.email = profile.getEmail();
+    $scope.google_token = authResp.id_token;
+
+    console.log('Full Name: ' + profile.getName());
+    console.log("Email: " + profile.getEmail());
+    console.log("ID Token: " + authResp.id_token);
+  };
+
   $scope.createUser = function (credentials) {
     var user = new User({
       "name": $scope.name,
@@ -26,4 +40,5 @@ signupController.controller('SignupCtrl', ['$scope', 'User', function ($scope, U
       $scope.bio = '';
     });
   };
+
 }]);
