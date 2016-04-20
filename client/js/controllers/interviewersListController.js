61557eb6054d5e0c8339ff9d73ec5b4b72c162fe
Joshin <JoshuaShin96@gmail.com>
2016-04-20 12:58:44 -0400

    Hard coded interviewer availability

--- client/js/controllers/interviewersListController.js
+++ client/js/controllers/interviewersListController.js
@@ -4,6 +4,34 @@ interviewersListController.controller('IntListCtrl', ['$scope', 'Interviewer', f
 
   Interviewer.query(function (results) {
     $scope.interviewers = results;
+
+
+	  var availability = [
+	    {
+	        "id": 1461168524308,
+	        "start": "2016-04-21T18:00:00.000Z",
+	        "end": "2016-04-21T20:00:00.000Z"
+	    },
+	    {
+	        "id": 1461168524309,
+	        "start": "2016-04-22T19:00:00.000Z",
+	        "end": "2016-04-22T21:00:00.000Z"
+	    },
+	    {
+	        "id": 1461168524310,
+	        "start": "2016-04-23T20:00:00.000Z",
+	        "end": "2016-04-23T22:00:00.000Z"
+	    }
+	];
+
+
+	$scope.interviewers.forEach(function(interviewer) {
+		interviewer.availability = availability;
+	});
   });
 
+
+
+
+  
 }]);
