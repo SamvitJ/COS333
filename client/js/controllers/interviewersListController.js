4c1b72294a5629bbe07917dc39e31d62c41dabe4
Joshin <JoshuaShin96@gmail.com>
2016-04-21 13:39:36 -0400

    Parse availabiilty data and display relevant info in modal

--- client/js/controllers/interviewersListController.js
+++ client/js/controllers/interviewersListController.js
@@ -5,49 +5,46 @@ interviewersListController.controller('IntListCtrl', ['$scope', 'User', function
   User.interviewers.query(function (results) {
     $scope.interviewers = results;
 
-
-
-
-
+    //hard coded data
 	var availability = [
 	    {
 	        "id": 1461168524308,
 	        "start": "2016-04-21T18:00:00.000Z",
-	        "end": "2016-04-21T20:00:00.000Z"
+	        "end": "2016-04-21T19:00:00.000Z"
 	    },
 	    {
 	        "id": 1461168524309,
-	        "start": "2016-04-22T19:00:00.000Z",
-	        "end": "2016-04-22T21:00:00.000Z"
+	        "start": "2016-04-27T09:00:00.000Z",
+	        "end": "2016-04-22T20:00:00.000Z"
 	    },
 	    {
 	        "id": 1461168524310,
-	        "start": "2016-04-23T20:00:00.000Z",
-	        "end": "2016-04-23T22:00:00.000Z"
+	        "start": "2016-04-28T20:00:00.000Z",
+	        "end": "2016-04-23T21:00:00.000Z"
 	    }
 	];
 
 	var currentTime = new Date();
 	$scope.interviewers.forEach(function(interviewer) {
-		// interviewer.availability = availability;
+		schedule = [];
+		for (i = 0; i < 7; i++) {
+			schedule.push([]);
+		}
 
 		availability.forEach(function(time) {
 			var start = new Date(time.start)
-			var end = new Date(time.end)
+
 			//store as available hours e.g. [4, 6, 7, 12, 13, 14] is 4-5am, 6-8am, 12pm-3pm
-			if (start > currentTime) {
-				console.log(start.getHours())
+			var timeDiff = start.getTime() - currentTime.getTime();
+			var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); // milliseconds
+
+			//check if date is within a one week range
+			if (diffDays > 0 && diffDays <= 7) {
+				schedule[diffDays - 1].push(start.getHours())
 			}
 		});
+		interviewer.availability = schedule;
 	});
   });
-
-  $scope.getDates = function (interviewer) {
-  	console.log("getDates")
-  	console.log(interviewer)
-  } 
-
-
-
 
 }]);
