86931f5317716cad9c90eeffe135d2bb323a4730
Joshin <JoshuaShin96@gmail.com>
2016-04-24 00:53:08 -0400

    Logic for updating interviewer's availability

--- client/js/controllers/interviewersListController.js
+++ client/js/controllers/interviewersListController.js
@@ -5,32 +5,32 @@ interviewersListController.controller('IntListCtrl', ['$scope', 'User', function
   User.interviewers.query(function (results) {
   	var currentTime = new Date();
   	results.forEach(function(interviewer) {
-  		var availability = interviewer.availability;
 
-		var schedule = [];
-		for (i = 0; i < 7; i++) {
-			schedule.push([]);
-		}
-
-		availability.forEach(function(time) {
-			var start = new Date(time.start)
-
-			//store as available hours e.g. [4, 6, 7, 12, 13, 14] is 4-5am, 6-8am, 12pm-3pm
-			var timeDiff = start.getTime() - currentTime.getTime();
-			var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); // milliseconds
-
-			//check if date is within a one week range
-			if (diffDays > 0 && diffDays <= 7) {
-				schedule[diffDays - 1].push({id: time.id, time: start.getHours()})
+			var schedule = [];
+			for (i = 0; i < 7; i++) {
+				schedule.push([]);
 			}
-		});
 
-		interviewer.schedule = schedule;
+			interviewer.availability.forEach(function(time) {
+				var start = new Date(time.start)
+
+				//store as available hours e.g. [4, 6, 7, 12, 13, 14] is 4-5am, 6-8am, 12pm-3pm
+				var timeDiff = start.getTime() - currentTime.getTime();
+				var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); // milliseconds
+
+				//check if date is within a one week range
+				if (diffDays > 0 && diffDays <= 7) {
+					schedule[diffDays - 1].push({id: time.id, time: start.getHours()})
+				}
+			});
+
+			interviewer.schedule = schedule;
   	})
+
     $scope.interviewers = results;
-
-
   });
+
+
 	$scope.schedule = {
 		selectedInterviewer: {},
 		selectedTime: {}
@@ -39,6 +39,17 @@ interviewersListController.controller('IntListCtrl', ['$scope', 'User', function
 	$scope.scheduleInterview = function(ev) {
 		console.log($scope.schedule.selectedInterviewer);
 		console.log($scope.schedule.selectedTime);
+
+		// update availability
+		var schedule = $scope.schedule.selectedInterviewer.availability;
+		var eventID = $scope.schedule.selectedTime.id;
+		var index = -1;
+		for (var i = 0; i < schedule.length; i++) {
+			if (schedule[i].id == eventID) {
+				index = i;
+			}
+		}
+		schedule.splice(index, 1);
 	}
 
 	$scope.updateSelectedInterviewer = function(index) {
