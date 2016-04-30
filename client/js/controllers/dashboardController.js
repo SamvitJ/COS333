058aaeb6f2b18c52bb9d6d6f3fc9cf422e0381f4
Joshin <JoshuaShin96@gmail.com>
2016-04-30 16:55:41 -0400

    Display actual interviews properly formatted

--- client/js/controllers/dashboardController.js
+++ client/js/controllers/dashboardController.js
@@ -5,7 +5,10 @@ dashboardController.controller('DashboardCtrl', ['$scope', '$sessionStorage', 'I
   $scope.user=$sessionStorage.name;
 
   Interview.interviews.query({google_token: $sessionStorage.google_token}, function(results) {
+  	results.forEach(function(result) {
+  		var start = new Date(result.start);
+  		result.start = start.toLocaleDateString('en-us', {weekday:'long', month:'short', day:'numeric', hour:'2-digit', minute: '2-digit'});
+  	})
 		$scope.interviews = results;
-		// console.log(results[0])
 	});
 }]);
