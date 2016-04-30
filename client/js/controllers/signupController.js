96a149b357bc1e9453f35b6016c83e4ae09d2c1a
henryshangguan <henry.shangguan@gmail.com>
2016-04-30 16:51:08 -0400

    Fixed syntax highlighting and reverted app height

--- client/js/controllers/signupController.js
+++ client/js/controllers/signupController.js
@@ -12,14 +12,17 @@ signupController.controller('SignupCtrl', ['$scope', '$sessionStorage', 'User', 
     $scope.name = profile.getName();
     $scope.email = profile.getEmail();
     $scope.google_token = authResp.id_token;
+    $scope.image_url = profile.getImageUrl();
 
     $sessionStorage.name = profile.getName();
     $sessionStorage.email = profile.getEmail();
     $sessionStorage.google_token = authResp.id_token;
+    $sessionStorage.image_url = profile.getImageUrl();
 
     console.log('Full Name: ' + profile.getName());
     console.log("Email: " + profile.getEmail());
     console.log("ID Token: " + authResp.id_token);
+    console.log("Image URL: " + profile.getImageUrl());
   };
 
   $scope.createUser = function (credentials) {
@@ -28,12 +31,14 @@ signupController.controller('SignupCtrl', ['$scope', '$sessionStorage', 'User', 
       "name": $scope.name,
       "email": $scope.email,
       "google_token": $scope.google_token,
+      "image_url": $scope.image_url,
       "interviewer": false
     });
     user.$save(function (result) {
       $scope.name = '';
       $scope.email = '';
       $scope.google_token = '';
+      $scope.image_url = '';
 
       window.location.href="#/dashboard";
     });
