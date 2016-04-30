070ee4bfb997f564f58b902442be15fa58d814c1
Joshin <JoshuaShin96@gmail.com>
2016-04-30 16:44:06 -0400

    Query for interviews for logged in user

--- client/js/controllers/dashboardController.js
+++ client/js/controllers/dashboardController.js
@@ -1,6 +1,11 @@
 var dashboardController = angular.module('dashboardController', []);
 
-dashboardController.controller('DashboardCtrl', ['$scope', '$sessionStorage', '$resource', function ($scope, $sessionStorage, $resource) {
+dashboardController.controller('DashboardCtrl', ['$scope', '$sessionStorage', 'Interview', '$resource', function ($scope, $sessionStorage, Interview, $resource) {
 
-   $scope.user=$sessionStorage.name;
+  $scope.user=$sessionStorage.name;
+
+  Interview.interviews.query({google_token: $sessionStorage.google_token}, function(results) {
+		$scope.interviews = results;
+		// console.log(results[0])
+	});
 }]);
