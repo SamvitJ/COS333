9a453053a9a5b446b18c8ff63e045f0e46ab00ac
SamvitJ <samvit.jain@gmail.com>
2016-05-01 17:04:15 -0400

    Minor indendation fixes.

--- client/js/controllers/dashboardController.js
+++ client/js/controllers/dashboardController.js
@@ -2,6 +2,6 @@ var dashboardController = angular.module('dashboardController', []);
 
 dashboardController.controller('DashboardCtrl', ['$scope', '$sessionStorage', '$resource', function ($scope, $sessionStorage, $resource) {
 
-   $scope.user=$sessionStorage.name;
-   $scope.image_url=$sessionStorage.image_url;
+    $scope.user=$sessionStorage.name;
+    $scope.image_url=$sessionStorage.image_url;
 }]);
